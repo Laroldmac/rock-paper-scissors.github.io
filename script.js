f6850c7e98fadc907e717039e398b7f9495bff6d
Laroldmac <148687199+Laroldmac@users.noreply.github.com>
2023-11-10 15:10:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+<script>
+    function playGame(playerChoice) {
+        var choices = ["rock", "paper", "scissors"];
+        var computerChoice = choices[Math.floor(Math.random() * 3)];
+
+        var result = "";
+
+        if (playerChoice === computerChoice) {
+            result = "It's a tie!";
+        } else if (
+            (playerChoice === "rock" && computerChoice === "scissors") ||
+            (playerChoice === "paper" && computerChoice === "rock") ||
+            (playerChoice === "scissors" && computerChoice === "paper")
+        ) {
+            result = "You win!";
+        } else {
+            result = "You lose!";
+        }
+
+        document.getElementById("result").innerHTML = "You chose " + playerChoice + ". The computer chose " + computerChoice + ". " + result;
+    }
+</script>
 
