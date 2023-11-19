2970131417bca5f4de385f3d07ace08bfd520c6e
Laroldmac <148687199+Laroldmac@users.noreply.github.com>
2023-11-19 19:33:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,37 +6,41 @@ function playGame(playerChoice) {
     var results = ""; // To store the results for all rounds
 
     if (roundsPlayed < 3) {
-        roundsPlayed++;
-        playerChoices.push(playerChoice);
+        var confirmation = window.confirm("You have chosen: " + playerChoice + "!");
+        
+        if (confirmation) {
+            roundsPlayed++;
+            playerChoices.push(playerChoice);
 
-        for (var i = 0; i < 3; i++) {
-            var computerChoice = choices[Math.floor(Math.random() * 3)];
-            var result = "";
+            for (var i = 0; i < 3; i++) {
+                var computerChoice = choices[Math.floor(Math.random() * 3)];
+                var result = "";
 
-            if (playerChoices[i] === computerChoice) {
-                result = "It's a tie!";
-            } else if (
-                (playerChoices[i] === "rock" && computerChoice === "scissors") ||
-                (playerChoices[i] === "paper" && computerChoice === "rock") ||
-                (playerChoices[i] === "scissors" && computerChoice === "paper")
-            ) {
-                result = "You win!";
-            } else {
-                result = "You lose!";
+                if (playerChoices[i] === computerChoice) {
+                    result = "It's a tie!";
+                } else if (
+                    (playerChoices[i] === "rock" && computerChoice === "scissors") ||
+                    (playerChoices[i] === "paper" && computerChoice === "rock") ||
+                    (playerChoices[i] === "scissors" && computerChoice === "paper")
+                ) {
+                    result = "You win!";
+                } else {
+                    result = "You lose!";
+                }
+
+                // Store each round's result separately
+                results += "Round " + (i + 1) + ": You chose " + playerChoices[i] + ". The computer chose " + computerChoice + ". " + result + "<br>";
             }
 
-            // Store each round's result separately
-            results += "Round " + (i + 1) + ": You chose " + playerChoices[i] + ". The computer chose " + computerChoice + ". " + result + "<br>";
-        }
+            // Display all three rounds' results only after three rounds
+            if (roundsPlayed === 3) {
+                document.getElementById("result").innerHTML = results;
 
-        // Display all three rounds' results only after three rounds
-        if (roundsPlayed === 3) {
-            document.getElementById("result").innerHTML = results;
-
-            // Disable buttons after three rounds
-            document.getElementById("rockBtn").disabled = true;
-            document.getElementById("paperBtn").disabled = true;
-            document.getElementById("scissorsBtn").disabled = true;
+                // Disable buttons after three rounds
+                document.getElementById("rockBtn").disabled = true;
+                document.getElementById("paperBtn").disabled = true;
+                document.getElementById("scissorsBtn").disabled = true;
+            }
         }
     }
 }
