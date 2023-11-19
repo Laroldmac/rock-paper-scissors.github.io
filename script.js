33c7c45f1a6705ca197c9b162cd0f37c18beec32
Laroldmac <148687199+Laroldmac@users.noreply.github.com>
2023-11-19 14:54:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function playGame(playerChoice) {
     var choices = ["rock", "paper", "scissors"];
-    var result = "";
+    var results = ""; // Accumulate results for all rounds
 
     for (var i = 0; i < 3; i++) {
         var computerChoice = choices[Math.floor(Math.random() * 3)];
+        var result = "";
 
         if (playerChoice === computerChoice) {
             result = "It's a tie!";
@@ -16,6 +17,9 @@ function playGame(playerChoice) {
         } else {
             result = "You lose!";
         }
-    document.getElementById("result").innerHTML = "You chose " + playerChoice + ". The computer chose " + computerChoice + ". " + result;
-}
+
+        results += "Round " + (i + 1) + ": You chose " + playerChoice + ". The computer chose " + computerChoice + ". " + result + "<br>";
+    }
+
+    document.getElementById("result").innerHTML = results; // Display results for all rounds
 }
